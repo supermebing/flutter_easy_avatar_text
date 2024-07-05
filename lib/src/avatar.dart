f8a25bbc953aa65086f404ca426cb76511a8646e
chenrubing <chenrubing@yaodaitong.com.cn>
2024-07-05 14:10:09 +0800

    更新支持中文名字

--- lib/src/avatar.dart
+++ lib/src/avatar.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
@@ -71,6 +72,10 @@ class Avatar extends StatelessWidget {
   /// 头像的可选外边框。
   final Border? border;
 
+  ///  is print
+  /// 是否打印
+  final bool isConsole;
+
   const Avatar({
     super.key,
     this.size = 100,
@@ -87,6 +92,7 @@ class Avatar extends StatelessWidget {
     this.upperCase = false,
     this.wordsCount,
     this.fileImage,
+    this.isConsole = false,
   });
 
   @override
@@ -184,10 +190,21 @@ class Avatar extends StatelessWidget {
 
   // 获取需要显示的字符串
   String _getDisplayStr() {
-    String str = String.fromCharCodes(
-      text.runes.toList(),
-    );
+    // String str = String.fromCharCodes(
+    //   text.runes.toList(),
+    // );
+    String str = text.trim();
+    // 如果是中文，则根据字符数量返回相应的字符串
+    if (RegExp(r'[\u4e00-\u9fa5]').hasMatch(str)) {
+      if (str.length == 2) {
+        return str;
+      } else if (str.length >= 3) {
+        return str.substring(str.length - 2);
+      }
+      return str; // 处理长度为1的情况
+    }
 
+    // 否则，按原有逻辑处理
     str = upperCase ? str.toUpperCase() : str;
     List<String> charList = str.trim().split(' ');
 
@@ -195,8 +212,12 @@ class Avatar extends StatelessWidget {
     if (charList.length > 1 && charList.length == wordsCount) {
       // 返回charList的第一个和第二个元素的首字母
       return '${charList[0][0].trim()}${charList[1][0].trim()}';
+    } else if (charList.length >= 3) {
+      return '${charList[charList.length - 2][0].trim()}${charList[charList.length - 1][0].trim()}';
     }
-    debugPrint('AvatarText ====== ${str[0]}');
+    if (isConsole) {
+      debugPrint('AvatarText ====== ${str[0]}');
+    }
     // 否则，返回newText的首字母
     return str[0];
   }
@@ -205,43 +226,55 @@ class Avatar extends StatelessWidget {
   Color _getBgColor() {
     if (backgroundColor != null) {
       return backgroundColor!;
-    } else if (textMode &&
-        RegExp(r'[A-Z]|').hasMatch(
-          _getDisplayStr(),
-        )) {
-      var colorKey = _getDisplayStr()[0].toLowerCase().toString();
-      var colorMap = {
-        "a": const Color.fromARGB(255, 255, 110, 94),
-        "b": const Color.fromARGB(255, 255, 108, 157),
-        "c": const Color.fromARGB(255, 236, 128, 255),
-        "d": const Color.fromARGB(255, 180, 134, 255),
-        "e": const Color.fromARGB(255, 144, 159, 255),
-        "f": const Color.fromARGB(255, 95, 153, 255),
-        "g": const Color.fromARGB(255, 74, 198, 255),
-        "h": const Color.fromARGB(255, 77, 234, 255),
-        "i": const Color.fromARGB(255, 60, 255, 235),
-        "j": const Color.fromARGB(255, 255, 189, 67),
-        "k": const Color.fromARGB(255, 191, 255, 118),
-        "l": const Color.fromARGB(255, 97, 69, 224),
-        "m": const Color.fromARGB(255, 255, 225, 57),
-        "n": const Color.fromARGB(255, 219, 178, 27),
-        "o": const Color.fromARGB(255, 243, 164, 7),
-        "p": const Color.fromARGB(255, 255, 132, 87),
-        "q": const Color.fromARGB(255, 204, 204, 204),
-        "r": const Color.fromARGB(255, 169, 194, 207),
-        "s": const Color.fromARGB(255, 161, 106, 83),
-        "t": const Color.fromARGB(255, 156, 156, 156),
-        "u": const Color.fromARGB(255, 169, 175, 224),
-        "v": const Color.fromARGB(255, 198, 164, 255),
-        "w": const Color.fromARGB(255, 214, 214, 214),
-        "x": const Color.fromARGB(255, 132, 241, 255),
-        "y": const Color.fromARGB(255, 216, 169, 153),
-        "z": const Color.fromARGB(255, 130, 231, 99),
-      };
+    } else {
+      String displayStr = _getDisplayStr();
+      if (RegExp(r'[\u4e00-\u9fa5]').hasMatch(displayStr)) {
+        // 中文字符，可以根据其编码生成随机颜色或者使用预定义的颜色
+        int codePoint = displayStr.codeUnitAt(0);
+        Random random = Random(codePoint);
+        // 示例：使用随机颜色
+        return Color.fromARGB(
+          255,
+          100 + random.nextInt(156), // 控制颜色明亮度
+          100 + random.nextInt(156),
+          100 + random.nextInt(156),
+        );
+      } else if (textMode &&
+          RegExp(r'[A-Z]|').hasMatch(
+            _getDisplayStr(),
+          )) {
+        var colorKey = _getDisplayStr()[0].toLowerCase().toString();
+        var colorMap = {
+          "a": const Color.fromARGB(255, 255, 110, 94),
+          "b": const Color.fromARGB(255, 255, 108, 157),
+          "c": const Color.fromARGB(255, 236, 128, 255),
+          "d": const Color.fromARGB(255, 180, 134, 255),
+          "e": const Color.fromARGB(255, 144, 159, 255),
+          "f": const Color.fromARGB(255, 95, 153, 255),
+          "g": const Color.fromARGB(255, 74, 198, 255),
+          "h": const Color.fromARGB(255, 77, 234, 255),
+          "i": const Color.fromARGB(255, 60, 255, 235),
+          "j": const Color.fromARGB(255, 255, 189, 67),
+          "k": const Color.fromARGB(255, 191, 255, 118),
+          "l": const Color.fromARGB(255, 97, 69, 224),
+          "m": const Color.fromARGB(255, 255, 225, 57),
+          "n": const Color.fromARGB(255, 219, 178, 27),
+          "o": const Color.fromARGB(255, 243, 164, 7),
+          "p": const Color.fromARGB(255, 255, 132, 87),
+          "q": const Color.fromARGB(255, 204, 204, 204),
+          "r": const Color.fromARGB(255, 169, 194, 207),
+          "s": const Color.fromARGB(255, 161, 106, 83),
+          "t": const Color.fromARGB(255, 156, 156, 156),
+          "u": const Color.fromARGB(255, 169, 175, 224),
+          "v": const Color.fromARGB(255, 198, 164, 255),
+          "w": const Color.fromARGB(255, 214, 214, 214),
+          "x": const Color.fromARGB(255, 132, 241, 255),
+          "y": const Color.fromARGB(255, 216, 169, 153),
+          "z": const Color.fromARGB(255, 130, 231, 99),
+        };
 
-      return colorMap.containsKey(colorKey)
-          ? colorMap[colorKey]!
-          : const Color.fromARGB(255, 82, 82, 82);
+        return colorMap.containsKey(colorKey) ? colorMap[colorKey]! : const Color.fromARGB(255, 82, 82, 82);
+      }
     }
     return const Color.fromARGB(255, 82, 82, 82);
   }
